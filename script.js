0fd3897ba9887b3296bd533e4d3e2fbfddb208df
Kolawole26 <kolenzy26@yahoo.com>
2021-10-21 17:39:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    hamburgerBtn = document.querySelector('.hamburger')
+    closeButton = document.querySelector('.close-btn')
+    mobileMenu = document.querySelector('.mobile-menu')
+
+
+    function showMenu() {
+        mobileMenu.classList.toggle('show');
+        closeButton.classList.toggle('show');
+        hamburgerBtn.classList.toggle('hide');
+    }
+
+    hamburgerBtn.addEventListener('click', showMenu)
+    closeButton.addEventListener('click', showMenu)
+})
